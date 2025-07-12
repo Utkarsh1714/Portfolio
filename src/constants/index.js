ac1289157a833018a4ca8b4eea3c332714fef8f5
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-07-12 23:26:10 +0530

    Updated my work expeirence

--- src/constants/index.js
+++ src/constants/index.js
@@ -163,38 +163,38 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Web Developer",
+    job: "Web Developer & Social Media Handling",
+    date: "March 2025",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+      "Contributed to the company's digital transformation by developing and maintaining internal and external websites.",
+      "Collaborated with the digital marketing team to assist in website development for promotional campaigns.",
+      "Managed and updated social media content to ensure consistent brand communication.",
+      "Designed and created digital content for product showcases and brand awareness.",
+      "Provided development support for the Quick Commerce platform used in product distribution.",
     ],
   },
-  {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
-    contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
-    ],
-  },
-  {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
-    contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
-    ],
-  },
+  // {
+  //   title: "Back-End Developer",
+  //   job: "Car Manufacture",
+  //   date: "2023-2024",
+  //   contents: [
+  //     "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+  //     "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+  //     "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+  //     "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+  //     "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+  //   ],
+  // },
+  // {
+  //   title: "Freelance Developer",
+  //   job: "Self-Employed",
+  //   date: "2025-Present",
+  //   contents: [
+  //     "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+  //     "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+  //   ],
+  // },
 ];
 export const reviews = [
   {
